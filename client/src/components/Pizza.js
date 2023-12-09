33a7db244c73c7ece1329afa5b17d373c76612bb
HaroonMalik771 <itsharoonmalik143@gmail.com>
2023-12-09 16:53:12 +0500

    client_site_updated

---
+++ client/src/components/Pizza.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function Pizza({ pizza }) {
+    return (
+        <div>
+            <h1>{pizza.name}</h1>
+            <img src={pizza.image} className='img-fluid' style={{ height: '200px', width: '200px' }} />
+
+            <div className='flex-container'>
+
+
+                <div className='w-100'>
+                 <p>Varients</p>
+                 <select>
+                    {pizza.varients.map(varient=>{
+                        return <option value={varient}>{varient}</option>
+                    })}
+                 </select>
+                </div>
+
+                <div className='w-100'>
+                <p>Quantity</p>
+                <select>
+                    {[...Array(10).keys()].map((x,i)=>{
+                        return <option value={i+1}>{i+1}</option>
+                    })}
+                    
+                </select>
+                </div>
+
+
+
+
+
+            </div>
+
+        </div>
+    )
+}
---
+++ client/src/pizzasdata.js
@@ -0,0 +1,89 @@
+const pizzas =[
+    {
+        name: "PEPPER BARBECUE CHICKEN",
+        varients: ["small","medium","large"],
+        prices:[
+            {
+            small: 400,
+            medium: 750,
+            large: 1000,
+            },
+        ],
+        category: "nonveg",
+        image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+        description: "Pepper Barbeque Chicken Cheese Pizza",
+    },
+    {
+        name: "NON VEG SUPREME",
+        varients: ["small","medium","large"],
+        prices:[
+            {
+            small: 400,
+            medium: 750,
+            large: 1000,
+            },
+        ],
+        category: "nonveg",
+        image: "https://i.pinimg.com/1200x/68/41/7b/68417b1de68f79f814d016b9f9881bd7.jpg",
+        description: "Bite into Supreme delight of Black Olives, Grilled Mushrooms and Chicken",
+
+    },
+    {
+        name: "GOLDEN CORN PIZZA",
+        varients: ["small","medium","large"],
+        prices:[
+            {
+            small: 400,
+            medium: 750,
+            large: 1000,
+            },
+        ],
+        category: "veg",
+        image: "https://www.crazymasalafood.com/wp-content/images/golden-1.jpg",
+        description: "Corn over the base makes it look beautiful. It serverd with tomato sauce and chills flakes",
+    },
+    {
+        name: "JALAPENO $ RED PAPRIKA PIZZA ",
+        varients: ["small","medium","large"],
+        prices:[
+            {
+            small: 400,
+            medium: 750,
+            large: 1000,
+            },
+        ],
+        category: "veg",
+        image: "https://www.crazymasalafood.com/wp-content/images/jalepeno.jpg",
+        description: "This pizza is amazing and can become more delicious if we will add more cheese in it.",
+    },
+    {
+        name: "MARGHERITA",
+        varients: ["small","medium","large"],
+        prices:[
+            {
+            small: 400,
+            medium: 750,
+            large: 1000,
+            },
+        ],
+        category: "veg",
+        image: "https://cdn.loveandlemons.com/wp-content/uploads/2019/09/margherita-pizza-500x500.jpg",
+        description: "This pizza base is made by mixing yeast, sugar, olive oil, salt and all-purpose flour.", 
+    },
+    {
+        name: "DOUBLE CHEESE MARGHERITA PIZZA",
+        varients: ["small","medium","large"],
+        prices:[
+            {
+            small: 400,
+            medium: 750,
+            large: 1000,
+            },
+        ],
+        category: "veg",
+        image: "https://www.crazymasalafood.com/wp-content/images/double-1.jpg",
+        description: "This is plain pizza which have cheese on it and become more delecious when we add paprika on it.",
+    },
+   
+];
+export default pizzas
