5d512ff929661456c5aa3f0e59c43e91d61d3cb5
HaroonMalik771 <itsharoonmalik143@gmail.com>
2023-12-16 18:40:15 +0500

    update

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import {useSelector,useDispatch} from 'react-redux'
 
 export default function CartScreen() {
-  return (
-    <div>Cart Screen</div>
-  )
+
+const cartstate = useSelector(state => state.addToCartReducer)
+const cartItem = cartstate.cartItems
+
+    return (
+        <div>
+
+            <div className='row justify-content-center'>
+                <div className='col-md-6'>
+                    <h1 style={{fontSize:'30px'}} > My Cart</h1>
+
+                </div>
+                <div className='col-md-4'></div>
+            </div>
+
+        </div>
+    )
 }
